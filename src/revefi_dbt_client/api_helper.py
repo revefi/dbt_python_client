6226c68f22f10e3df210cbad3f2931deede6eee6
sumeet-revefi <sumeet@revefi.com>
2023-10-15 20:05:09 -0700

    Add --ignore-error flag to exit with 0 even if an error occurs

--- src/revefi_dbt_client/api_helper.py
+++ src/revefi_dbt_client/api_helper.py
@@ -17,16 +17,17 @@ class MakeApiCall:
         encoded_string = encoded_bytes.decode('utf-8')
 
         if len(contents) > 64 * 1024 * 1024:
-            self.log.error("File size exceeds 64 MB. Please try again with a smaller file size.")
-            return
+            raise ValueError("File size exceeds 64 MB. Please try again with a smaller file size")
+
         response = requests.post(f"{api}",
                                  json={'token': token, 'hash': hash, 'contents': [encoded_string]})
 
-        if response.status_code == 200:
-            self.log.info("Upload successful.")
-        else:
-            self.log.error(f"[{response.status_code}] Unable to upload - {response}")
-            return
+        if response.status_code != 200:
+            raise Exception("Error uploading file", {
+                'error': response.text
+            })
+
+        self.log.info("Upload successful: {}".format(response.text))
 
     def __init__(self, api, token, contents):
         self.get_data(api, token, contents)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -8,14 +8,16 @@ def test_parse_args_legacy():
     assert args.project_folder == '/path/to/project/dir'
     assert args.target_folder is None
     assert args.logs_folder is None
+    assert args.ignore_errors is False
 
-    argv = 'dbt --token definitely-a-real-token --project_folder /path/to/project/dir --target_folder custom_target_dir --logs_folder /path/to/logs/dir'.split(
+    argv = 'dbt --token definitely-a-real-token --project_folder /path/to/project/dir --target_folder custom_target_dir --logs_folder /path/to/logs/dir --ignore-error'.split(
         ' ')
     args = parse_args_legacy(argv)
     assert args.token == 'definitely-a-real-token'
     assert args.project_folder == '/path/to/project/dir'
     assert args.target_folder == 'custom_target_dir'
     assert args.logs_folder == '/path/to/logs/dir'
+    assert args.ignore_errors is True
 
 
 def test_parse_args_v2():
@@ -25,11 +27,13 @@ def test_parse_args_v2():
     assert args.project_folder == '/path/to/project/dir'
     assert args.target_folder is None
     assert args.logs_folder is None
+    assert args.ignore_errors is False
 
-    argv = '--token definitely-a-real-token --project_folder /path/to/project/dir --target_folder custom_target_dir --logs_folder /path/to/logs/dir'.split(
+    argv = '--token definitely-a-real-token --project_folder /path/to/project/dir --target_folder custom_target_dir --logs_folder /path/to/logs/dir --ignore-error'.split(
         ' ')
     args = parse_args_v2(argv)
     assert args.token == 'definitely-a-real-token'
     assert args.project_folder == '/path/to/project/dir'
     assert args.target_folder == 'custom_target_dir'
     assert args.logs_folder == '/path/to/logs/dir'
+    assert args.ignore_errors is True
